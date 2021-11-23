ea399a640ca3242f0a033cfe2d2d816a55634291
Jesse Viau <jesse.viau92@stclairconnect.ca>
2021-11-23 03:21:56 -0500

    Initial Commit

---
+++ project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Jesse on 2021-11-23.
+//  Copyright © 2021 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
